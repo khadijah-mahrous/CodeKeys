677bb0b2dc7274c6dda6c041679f4917d0c08579
khadijah-mahrous <khadijahmm77@gmail.com>
2026-05-05 14:04:38 +0300

    app.py

--- app.py
+++ app.py
@@ -805,4 +805,4 @@ def change_password():
     
     return jsonify({"success": True, "message": "Password changed successfully"})
 if __name__ == '__main__':
-app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
